fd77ae65904f96296384f4c81c7b411618272fcf
Sausan Nabilah <76219106+Sausan18@users.noreply.github.com>
2021-06-02 22:38:45 +0700

    menambahkan file halaman utama yang telah selesai

--- index.php
+++ index.php
@@ -11,10 +11,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style-halaman-awal.css">
     <link rel="stylesheet" href="./assets/css/bootstrap.css">
-    <title>Index</title>
+    <title>Halaman Utama</title>
+    <link rel="icon" type="image/png" href="img/icons/icon-siagam.png">
+
+    <style>
+        footer{
+            font-family: 'Libre Franklin', sans-serif;
+            background-color: grey;
+            color: white;
+        }
+    </style>
 </head>
+    
 
 <body>
+<div class="alert alert-primary d-flex align-items-center" role="alert" style="margin-left: 515px; margin-right: 515px; z-index: 10; margin-top: 50px;">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+  </svg>
+  <div>
+    Mohon Mengambil Antrian Maksimal 1 Jam Sebelum Berkunjung
+  </div>
+</div>
+
     <div class="container bg-dark-index box">
 
         <div class="justify-content-center"><br>
@@ -37,8 +56,8 @@
 
     </div>
 
-    <p style="text-align: center; margin-top: 0px; color: white;">Ver 1.2</p>
-    <p style="margin: -20px 20px;">
+    <p style="text-align: center; margin-top: -5px; color: white;">Ver 1.4</p>
+    <p style="margin: -65px 20px;">
         <a href="support.php">
             <img src="img/icons/tanda-tanya.png" style="padding : 5px 5px; width: 40px;height: 40px;border-radius: 100%;" />
     </p>
